65b3bd9cca7fefa09a9c8290d82b5ebc6ac832f3
Claudia Pierson <c172@umbc.edu>
2020-02-02 07:35:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,29 @@
+katz_deli = []
+
 def line(katz_deli)
-  
-  if katz_deli.length == 0
-  puts "The line is currently empty."
-else
-  line_string = ""
-  katz_deli.each_with_index do |name, index|
-  line_string.<<("#{index}. #{name}")
-  return line_string
-end
- puts "The line is currently #{line_string.join("")}"
+  line_positions = [];
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.<<(name)
-  if katz_deli == 3
-  else 
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
