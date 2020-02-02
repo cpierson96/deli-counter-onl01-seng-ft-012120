59bca2844c5ca4f08ce0757a2e9e03f5ac766625
Claudia Pierson <c172@umbc.edu>
2020-02-02 07:32:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line(katz_deli)
+  
+  if katz_deli.length == 0
+  puts "The line is currently empty."
+else
+  line_string = ""
+  katz_deli.each_with_index do |name, index|
+  line_string.<<("#{index}. #{name}")
+  return line_string
+end
+ puts "The line is currently #{line_string.join("")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.<<(name)
+  if katz_deli == 3
+  else 
+end
+end
